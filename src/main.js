d93ba97390613924109f0dfa95ff532ec9c4f749
xiaojie <zhs010424@163.com>
2021-10-05 20:11:33 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入全局样式
+import './style/index.less'
+
+// 生产完成提示
 Vue.config.productionTip = false
 
+// 
 new Vue({
   router,
   store,
